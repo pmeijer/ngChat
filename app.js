1d9be9f5012e3afdb9c89888c07e349769a4b4f8
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2015-04-17 17:36:37 -0500

    Add supervisor ui and improve supervisor.

--- app.js
+++ app.js
@@ -1,18 +1,104 @@
 /**
  * @author lattmann / https://github.com/lattmann
  */
+'use strict';
 
 var ChatServer = require('./chat-server'),
     chatServer,
     chatServers = {},
 
     PORT = process.env.PORT || 8080,
+    debug = require('debug'),
+    logger = debug('ngChat:supervisor'),
+    express = require('express'),
+    app = express();
 
-    i;
+app.use('/', express.static(__dirname + '/public_app/'));
 
+app.get('/api/servers', function (req, res, next) {
+    var result = {},
+        key;
 
-for (i = 0; i < 5; i += 1) {
-    chatServer = new ChatServer(PORT + i);
-    chatServer.start();
-    chatServer.stop();
+    for (key in chatServers) {
+        if (chatServers.hasOwnProperty(key)) {
+            result[key] = {
+                port: key,
+                online: chatServers[key].isRunning()
+            };
+        }
+    }
+
+    res.json(result);
+});
+
+app.get('/api/servers/:port', function (req, res, next) {
+    var port = req.params.port;
+    if (chatServers.hasOwnProperty(port) === false) {
+        res.json(chatServers[port].isRunning());
+    } else {
+        res.sendStatus(200);
+    }
+});
+
+app.post('/api/servers/:port', function (req, res, next) {
+    var port = req.params.port;
+    if (chatServers.hasOwnProperty(port) === false) {
+        logger('creating server ' + port);
+        chatServers[port] = new ChatServer(port);
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(200);
+    }
+});
+
+app.delete('/api/servers/:port', function (req, res, next) {
+    var port = req.params.port;
+    if (chatServers.hasOwnProperty(port)) {
+        logger('deleting server ' + port);
+        if (chatServers[port].isRunning()) {
+            chatServers[port].stop();
+        }
+        delete chatServers[port];
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/api/servers/:port/start', function (req, res, next) {
+    var server = chatServers[req.params.port];
+    if (server) {
+        if (server.isRunning() === false) {
+            logger('starting server ' + req.params.port);
+            server.start();
+        }
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/api/servers/:port/stop', function (req, res, next) {
+    var server = chatServers[req.params.port];
+    if (server) {
+        if (server.isRunning()) {
+            logger('stopping server ' + req.params.port);
+            server.stop();
+        }
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.listen(PORT);
+logger('up and running on ' + PORT);
+
+var i,
+    port;
+
+for (i = 1; i < 5; i += 1) {
+    port = PORT + i;
+    chatServers[port] = new ChatServer(port);
+    chatServers[port].start();
 }
---
+++ public_app/module.js
@@ -0,0 +1,119 @@
+/*globals angular, io, alert */
+/**
+ * Created by pmeijer on 12/31/2014.
+ */
+
+angular.module('ngChatSupervisor', ['ngMaterial'])
+    .controller('SupervisorController', function ($rootScope, $scope, $interval, $http) {
+        'use strict';
+
+        $scope.address = 'http://localhost:8080';
+
+        $scope.servers = {};
+
+
+        $scope.addServer = function () {
+            $scope.servers[$scope.address] = {
+                address: $scope.address
+            };
+        };
+
+        $scope.removeServer = function (address) {
+            if ($scope.servers.hasOwnProperty(address)) {
+                delete $scope.servers[address];
+            }
+        };
+
+
+        $scope.startChatServer = function (address, port) {
+            $http.post(address + '/api/servers/' + port + '/start').
+                success(function (data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+
+                });
+        };
+
+        $scope.stopChatServer = function (address, port) {
+            $http.post(address + '/api/servers/' + port + '/stop').
+                success(function (data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+
+                });
+        };
+
+        $scope.deleteChatServer = function (address, port) {
+            $http.delete(address + '/api/servers/' + port).
+                success(function (data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+
+                });
+        };
+
+        $scope.createChatServer = function (address, port) {
+            $http.post(address + '/api/servers/' + port).
+                success(function (data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+
+                });
+        };
+
+
+        function updateServer(server) {
+            console.log('updating', server);
+            $http.get(server.address + '/api/servers').
+                success(function (data, status, headers, config) {
+                    var i;
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    server.data = data;
+                    server.status = status;
+                    server.newPort = server.newPort || 8086;
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    server.status = status;
+                });
+        }
+
+        $interval(function () {
+            var key,
+                server;
+
+            for (key in $scope.servers) {
+                if ($scope.servers.hasOwnProperty(key)) {
+                    server = $scope.servers[key];
+                    updateServer(server);
+                }
+            }
+
+        }, 2000);
+
+    })
+    .run(function($rootScope, $window) {
+        'use strict';
+    });
