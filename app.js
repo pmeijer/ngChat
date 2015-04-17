6b3475091eda9bb88fd72ddbf267a3ad6f8a8eb4
pmeijer <patrik85@isis.vanderbilt.edu>
2015-04-17 16:00:06 -0500

    adding logging and custom redis client

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-/*globals require, console, module, __dirname*/
+/*globals require, module, __dirname, process*/
+/*jshint node:true*/
 /**
  * Created by pmeijer on 12/30/2014.
  */
 
 var PORT = 8080,
     NUM_SERVERS = 5,
+    redisHost = process.env.REDIS_HOST || '127.0.0.1',
+    redisPort = 6379,
+    debug = require('debug'),
     i;
 
 var startServer = function (port) {
     'use strict';
     var express = require('express'),
         socketIO = require('socket.io'),
+        redis = require('redis'),
+        logger = debug('ngChat:' + port),
         io,
         adapter,
         server,
+        pub,
+        sub,
+        sockets = [],
         app = express();
 
     app.use('/', express.static(__dirname + '/public/'));
     server = app.listen(port);
     io = socketIO.listen(server);
-    adapter = require('socket.io-redis')({host:'127.0.0.1', port:6379});
+
+    pub = redis.createClient(redisPort, redisHost);
+    sub = redis.createClient(redisPort, redisHost, { detect_buffers: true });
+    pub.on('error', function (err) {
+        logger('pub', err);
+    });
+    sub.on('error', function (err) {
+        logger('sub', err);
+    });
+
+    adapter = require('socket.io-redis')({
+        host:redisHost,
+        port:redisPort,
+        pubClient: pub,
+        subClient: sub
+    });
     io.adapter(adapter);
-    var sockets = [];
 
     io.sockets.on('connection', function (socket) {
-        console.log(port + ' New client connected!');
+        logger('New client connected' + socket.id);
         sockets.push(socket);
         socket.emit('message', {
             message: 'Welcome to the chat',
@@ -32,30 +55,30 @@ var startServer = function (port) {
             timeStamp: (new Date()).toISOString()});
 
         socket.on('subscribe', function(room) {
-            console.log('====== JOIN =======');
-            console.log('Nbr of rooms: ' + Object.keys(io.nsps['/'].adapter.rooms).length);
-            console.log('joining room', socket.id);
+            logger('====== JOIN ' + room + '=======');
+            logger('Nbr of rooms        : ' + Object.keys(io.nsps['/'].adapter.rooms).length);
+            logger('joining room, socket:' + socket.id);
             socket.join(room);
-            console.log(room + ' has nbr of users: ' + Object.keys(io.nsps['/'].adapter.rooms[room]).length);
-            console.log('Nbr of rooms: ' + Object.keys(io.nsps['/'].adapter.rooms).length);
-            console.log('====== JOIN =======\n');
+            logger('Nbr of users        : ' + Object.keys(io.nsps['/'].adapter.rooms[room]).length);
+            logger('Nbr of rooms        : ' + Object.keys(io.nsps['/'].adapter.rooms).length);
+            logger('====== JOIN =======\n');
             //}
         });
 
         socket.on('unsubscribe', function(room) {
-            console.log('====== LEAVE =======');
-            console.log('Nbr of rooms: ' + Object.keys(io.nsps['/'].adapter.rooms).length);
-            console.log('leaving room: ', socket.id);
+            logger('====== LEAVE ' + room + '=======');
+            logger('Nbr of rooms        : ' + Object.keys(io.nsps['/'].adapter.rooms).length);
+            logger('leaving room, socket: ' + socket.id);
             socket.leave(room);
             if (io.nsps['/'].adapter.rooms[room]) {
-                console.log(room + ' has nbr of users: ' + Object.keys(io.nsps['/'].adapter.rooms[room]).length);
+                logger('Nbr of users      : ' + Object.keys(io.nsps['/'].adapter.rooms[room]).length);
             }
-            console.log('Nbr of rooms: ' + Object.keys(io.nsps['/'].adapter.rooms).length);
-            console.log('====== LEAVE =======\n');
+            logger('Nbr of rooms        : ' + Object.keys(io.nsps['/'].adapter.rooms).length);
+            logger('====== LEAVE =======\n');
         });
 
         socket.on('send', function(data) {
-            console.log(port, data.user, data.message, data.room);
+            logger(port, data.user, data.message, data.room);
             if (data.room) {
                 socket.broadcast.to(data.room).emit('message', data);
             } else {
@@ -65,15 +88,15 @@ var startServer = function (port) {
             }
         });
         
-        socket.on('disconnect', function() { 
-            console.log(port + ' ' + socket.id + ' disconnected');
+        socket.on('disconnect', function() {
+            logger(socket.id + ' disconnected');
         });
     });
     
-    console.log('listening on port: ', port);
+    logger('listening');
     if (port === PORT) {
         setTimeout(function () {
-            console.log('closing', PORT);
+            logger('closing');
             server.close();
             sockets.forEach(function(socket){
                 socket.disconnect(true);
